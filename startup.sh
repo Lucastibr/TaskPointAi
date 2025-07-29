799d9c758a5687a3d85405eff7b22133df58691e
Lucas Azevedo <lucastibr@outlook.com>
2025-07-29 19:08:22 -0300

    Ajustes startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Instalando ODBC Driver 17 para SQL Server..."
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
+
 echo "Instalando dependências..."
 pip install -r requirements.txt
 
