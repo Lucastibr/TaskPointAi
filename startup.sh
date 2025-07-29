8024696fba4c834a86798b5eca0dfea0a2c03464
Lucas Azevedo <lucastibr@outlook.com>
2025-07-29 18:53:33 -0300

    Melhorias projeto Again

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Instalando dependências..."
-pip install -r /home/site/wwwroot/requirements.txt
+pip install -r requirements.txt
 
 echo "Iniciando Gunicorn com UvicornWorker..."
 exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker api:app
